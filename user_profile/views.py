e118c59efcecb9bdacfce1970455d242ae814b19
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2021-01-23 16:51:12 -0500

    login protect profile

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from user_profile.models import UserProfile
 
 
+@login_required
 def view_profile(request, username):
-    user = UserProfile.objects.get(username=username)
-    context = {'name': 'Profile', 'project': 'Django', 'user': user}
+    user = User.objects.get(username=username)
+    profile = UserProfile.objects.get(user=user)
+    context = {'profile': profile}
     return render(request, 'profile.html', context)
