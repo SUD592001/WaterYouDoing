b8031627d767ff3d48c52f01aa95bacc303c4e78
HenryWellman <hdwellma@purdue.edu>
2021-01-23 10:04:08 -0500

    started save method with total score

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.urls import reverse
 
 
@@ -9,6 +10,11 @@ class UserProfile(models.Model):
 
     def profile_url(self):
         return reverse('view_profile', kwargs={'username': self.username})
+
+    def save(self):
+        score = 0 #whatever
+        super.save()
+        self.score = 0; #total water usage
 
     def __str__(self):
         return self.username
