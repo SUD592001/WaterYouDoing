6ef7a0c08b374da4bd0e290db65ec91813ec9b1e
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2021-01-23 14:34:41 -0500

    remove useless stuff

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -5,4 +5,8 @@ admin.site.site_title = 'Water U Doing'
 admin.site.site_header = 'Water U Doing // Admin Portal'
 
 # Register your models here.
-admin.site.register(models.UserProfile)
+@admin.register(models.UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    ordering = ['username']
+    list_display = ['username', 'score']
+    readonly_fields = ['score']
