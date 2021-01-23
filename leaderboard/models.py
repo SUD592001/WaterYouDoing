5d0202593abc78d20fb84023c0c5be77d8713935
HenryWellman <hdwellma@purdue.edu>
2021-01-22 22:10:15 -0500

    Updated LeaderBoard html

--- leaderboard/models.py
+++ leaderboard/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 Class Name(models.Model):
-    name = models.CharField(max_length=30, min_length=4, primary_key=True)
+    name = models.CharField(max_length=30, primary_key=True)
 
 Class Score(models.Model):
     score = models.IntegerField(No=false)
