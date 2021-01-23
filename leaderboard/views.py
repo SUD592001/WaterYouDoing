c34b7c0f6488ccabf33f9d1522aff27448fa1b0c
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2021-01-22 22:15:10 -0500

    user profile page

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from user_profile.models import UserProfile
+
 
 # Create your views here.
+def test(request):
+    users = UserProfile.objects.all().order_by('score')
+    print(users)
+    return HttpResponse(request, users[0])
