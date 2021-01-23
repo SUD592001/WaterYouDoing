c5f2c437c133dcd8799b85d8cad0fd4552098729
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2021-01-23 10:42:21 -0500

    custom admin

--- evaluator/views.py
+++ evaluator/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 from evaluator.forms import EvaluatorForm
 from user_profile.models import UserProfile
 
@@ -9,19 +10,32 @@ def evaluate(request):
         form = EvaluatorForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
-            # calculate score = laundry loads
-            score = int(data['weekly_laundry_loads'])
-            
             # if profile with given username already exists, update profile. 
             # otherwise make profile for user
             try:
                 profile = UserProfile.objects.get(username=data['username'])
-                profile.score = score
+                profile.weekly_laundry_loads = data['weekly_laundry_loads']
+                profile.daily_bathroom_trips = data['daily_bathroom_trips']
+                profile.weekly_showers = data['weekly_showers']
+                profile.shower_times = data['shower_times']
+                profile.weekly_baths = data['weekly_baths']
+                profile.weekly_dishes = data['weekly_dishes']
+                profile.weekly_sprinkler = data['weekly_sprinkler']
+                profile.swimming_pool = data['swimming_pool']
+                profile.score = 999
             except UserProfile.DoesNotExist:
-                profile = UserProfile(username=data['username'], score=score)
-            
+                profile = UserProfile(username=data['username'], weekly_laundry_loads = data['weekly_laundry_loads'],
+                                      daily_bathroom_trips = data['daily_bathroom_trips'],
+                                      weekly_showers = data['weekly_showers'], shower_times = data['shower_times'],
+                                      weekly_baths = data['weekly_baths'], weekly_dishes = data['weekly_dishes'],
+                                      weekly_sprinkler = data['weekly_sprinkler'], 
+                                      swimming_pool = data['swimming_pool'])
+                profile.score = 999
             # store profile in db
             profile.save()
+            # go to results page
+            return HttpResponseRedirect('/done/')
+            return HttpResponseRedirect(reverse('evaluation_result', username=data['username']))
     else:
         form = EvaluatorForm()
 
--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from user_profile import models
 
+admin.site.site_title = 'Water U Doing'
+admin.site.site_header = 'Water U Doing // Admin Portal'
 
 # Register your models here.
 admin.site.register(models.UserProfile)
