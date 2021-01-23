948bbe8204c609e7c5805b05b242e5cc0c4df782
HenryWellman <hdwellma@purdue.edu>
2021-01-23 01:09:45 -0500

    added questions to evalutation form

--- evaluator/forms.py
+++ evaluator/forms.py
@@ -8,16 +8,18 @@ class EvaluatorForm(forms.Form):
     daily_bathroom_trips = forms.IntegerField(label='On average, how many times do you go to the bathroom each day? ', 
                                                     required=True)
 
-    weekly_showers = forms.IntegerField(label='On average, how many showers do you take in a week? ', 
-                                                    required=True)
+    weekly_showers = forms.IntegerField(label='On average, how many showers do you take in a week? ', required=True)
 
-    shower_times = forms.IntegerField(label='On average, how long are your showers? (in minutes) ', 
-                                                    required=True)
+    shower_times = forms.IntegerField(label='On average, how long are your showers? (in minutes) ', required=True)
 
-    weekly_baths = forms.IntegerField(label='On average, how many baths do you take in a week? ', 
-                                                    required=True)
+    weekly_baths = forms.IntegerField(label='On average, how many baths do you take in a week? ', required=True)
 
     weekly_dishes = forms.IntegerField(label='On average, how many times do you wash your dishes in a week? ', 
                                                     required=True)
+
+    weekly_sprinkler = forms.IntegerField(label='On average, how many times do you water your lawn in a week? ', 
+                                                    required=True)
+
+    swimming_pool = forms.IntegerField(label='Do you have a swimming pool? ',  required=True)
 
     username = forms.CharField(label='Username', min_length=4, max_length=30, required=True)
