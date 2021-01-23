11c625a2cb1cfd59ab18375788e5dae5a2ccb868
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2021-01-22 22:04:54 -0500

    user profile models

---
+++ user_profile/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=30, null=False, primary_key=True)
+    bio = models.TextField(null=True, blank=True)
+    score = models.IntegerField(null=False)
--- waterudoing/settings.py
+++ waterudoing/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'evaluator',
+    'leaderboard',
+    'user_profile',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Indiana/Indianapolis'
 
 USE_I18N = True
 
