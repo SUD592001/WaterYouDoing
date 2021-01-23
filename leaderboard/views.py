4337de089c1107a5332e4e840eb98a8dfb05a489
HenryWellman <hdwellma@purdue.edu>
2021-01-22 22:06:37 -0500

    Started Basic leaderBoard html

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, 
 
 # Create your views here.
+def home(request):
+    #return HttpResponse("This is my homepage (/)")
+    context = {'name': 'LeaderBoard', 'project': 'Django'}
+    return render(request, 'leaderBoard.html', context)
