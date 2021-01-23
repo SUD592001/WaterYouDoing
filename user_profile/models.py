05c0cf789159dedc0ae5cd611c5bf9fd4ad49409
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2021-01-23 01:04:34 -0500

    view profile from leaderboard

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 
 class UserProfile(models.Model):
     username = models.CharField(max_length=30, null=False, primary_key=True)
     bio = models.TextField(null=True, blank=True)
     score = models.IntegerField(null=False)
+
+    def profile_url(self):
+        return reverse('view_profile', kwargs={'username': self.username})
 
     def __str__(self):
         return self.username
--- user_profile/urls.py
+++ user_profile/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from user_profile import views
 
 urlpatterns = [
-    path('<str:username>/', views.view_profile)
+    path('<str:username>/', views.view_profile, name='view_profile')
 ]
