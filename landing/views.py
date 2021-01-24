fd72fcef905b42b60f9d48aab503ac5971f51838
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2021-01-23 21:51:57 -0500

    add signup

--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.shortcuts import render, HttpResponseRedirect
 from django.conf import settings
+from django.urls import reverse
+from landing.forms import SignupForm
 
 
 def landing(request):
@@ -8,3 +12,23 @@ def landing(request):
         'avg_high': settings.AVG_WEEKLY_WATER_HI,
     }
     return render(request, 'home.html', context=context)
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        data = form.data
+        if form.is_valid():
+            # create a user if data is valid
+            data = form.cleaned_data
+            new_user = User.objects.create_user(data['username'], data['email'], data['password1'])
+            new_user.save()
+            # log user in
+            user = authenticate(username=data['username'], password=data['password1'])
+            login(request, user)
+            return HttpResponseRedirect(reverse('landing'))
+    else:
+        form = SignupForm()
+    
+    context = {'form': form}
+    return render(request, 'registration/signup.html', context=context)
