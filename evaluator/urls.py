9e21dd0c4b9116b9e6295d9490556618781f5e15
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2021-01-23 23:37:48 -0500

    fix learn more

--- evaluator/urls.py
+++ evaluator/urls.py
@@ -4,7 +4,7 @@ from evaluator import views
 urlpatterns = [
     path('new/', views.evaluate, name='evaluate'),
     path('detail/<str:username>/', views.detail, name='detail'),
+    path('result/learn-more/', views.learn_more, name='learn_more'),
     path('result/', views.result, name='result'),
     path('result/<str:username>/', views.result, name='result'),
-    path('result/learn-more/', views.learn_more, name='learn_more'),
 ]
